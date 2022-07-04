43b2d96534a8767d9bde6ec3535e4bfa4a1318c5
narmina <namira.lir@gmail.com>
2022-07-04 15:16:53 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from utils import candidates_list
+my_app = Flask(__name__)
+
+@my_app.route("/")
+def main_page():
+    candidates_new = ''
+    for candidate in candidates_list:
+        candidates_new += (f" Имя кандидата - {candidate['name']} \n Позиция кандидата {candidate['position']} \n Навыки {candidate['skills']} \n\n")
+    return '<pre>'+candidates_new+'</pre>'
+
+@my_app.route("/candidates/<int:x>")
+def candidates_page(x):
+    candidate_new = ''
+    for candidate in candidates_list:
+        if x == candidate['id']:
+            candidate_new += (f" Имя кандидата - {candidate['name']} \n Позиция кандидата {candidate['position']} \n Навыки {candidate['skills']} \n\n")
+            return f'<pre>{candidate_new}<pre><img src="{candidate["picture"]}">'
+
+@my_app.route("/skills/<skill>")
+def candidates_skills(skill):
+    candidates_skill = ''
+    for candidate in candidates_list:
+        skills_list = candidate['skills'].lower().replace(" ","").split(",")
+        if skill.lower() in skills_list:
+            candidates_skill += (f" Имя кандидата - {candidate['name']} \n Позиция кандидата {candidate['position']} \n Навыки {candidate['skills']} \n\n")
+    return f'<pre>{candidates_skill}<pre>'
+
+my_app.run()
---
+++ utils.py
@@ -0,0 +1,4 @@
+import json
+
+with open ("candidates.json") as file:
+    candidates_list = json.load(file)
